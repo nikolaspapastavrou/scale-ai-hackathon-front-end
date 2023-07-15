667fc600f99eefe18c3ff9dfd50e311881bd051e
nikolaspapastavrou <nikolaspapastavrou47@gmail.com>
2023-07-15 16:44:27 -0700

    test

--- components/prompt-form.js
+++ components/prompt-form.js
@@ -9,6 +9,7 @@ import sample from "lodash/sample";
 
 export default function PromptForm(props) {
   const [prompt] = useState(sample(samplePrompts));
+  const [image, setImage] = useState(null);
 
   return (
     <form
@@ -28,7 +29,7 @@ export default function PromptForm(props) {
           className="bg-black text-white rounded-r-md text-small inline-block px-3 flex-none"
           type="submit"
         >
-          Fix Bias
+          FixBias
         </button>
       </div>
     </form>
--- pages/index.js
+++ pages/index.js
@@ -98,7 +98,27 @@ export default function Home() {
           </div>
         </div>
         <div className="max-w-[512px] mx-auto">
-        <textarea name="comments" id="comments"></textarea>
+        <form
+      onSubmit={props.onSubmit}
+      className="py-5 animate-in fade-in duration-700"
+    >
+      <div className="flex max-w-[512px]">
+        <input
+          type="text"
+          defaultValue={prompt}
+          name="prompt"
+          placeholder="Enter a Hugging Face model name..."
+          className="block w-full flex-grow rounded-l-md"
+        />
+
+        <button
+          className="bg-black text-white rounded-r-md text-small inline-block px-3 flex-none"
+          type="submit"
+        >
+          Get Response
+        </button>
+      </div>
+    </form>
         </div>
       </main>
     </div>
