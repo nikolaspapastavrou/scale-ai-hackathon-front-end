b01b94c2ca1da3dfceaec9964a010add9f857247
nikolaspapastavrou <nikolaspapastavrou47@gmail.com>
2023-07-15 14:00:10 -0700

    valodation

--- pages/index.js
+++ pages/index.js
@@ -25,7 +25,7 @@ export default function Home() {
     console.log('Assembling url');
     console.log(e.target.prompt.value);
 
-    const huggingfaceUrl = `https://huggingface.co/${e.target.prompt.value}`
+    const huggingfaceUrl = `www.google.com`
 
     console.log(huggingfaceUrl);
   
