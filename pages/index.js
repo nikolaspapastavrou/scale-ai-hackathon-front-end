c20e7f84d84dae358f2c617d3143f87706744e2c
nikolaspapastavrou <nikolaspapastavrou47@gmail.com>
2023-07-15 16:55:23 -0700

    test

--- pages/index.js
+++ pages/index.js
@@ -113,8 +113,12 @@ export default function Home() {
           className="bg-black text-white rounded-r-md text-small inline-block px-3 flex-none"
           type="submit"
         >
-          Get Response
+          Test Prompt
         </button>
+
+        <canvas id="myCanvas" width="500" height="500" style="border:1px solid #000000;">
+            Your browser does not support the HTML5 canvas tag.
+        </canvas>
       </div>
     </form>
         </div>
